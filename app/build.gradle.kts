5f219fbb0c62fb7c76f248ccc4afffe158f7f205
ronakukani <ukanironak@gmail.com>
2024-04-25 18:59:17 +0530

    added base compose navigation and also added product and cart screen to navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.koin.android)
+    implementation(libs.androidx.navigation)
 
     implementation(project(":networking"))
 
